20096a4dcc7bde638aaf7d6507fea4bc5747aca1
AYESHAHANNURE25 <ayeshahannure.ah@gmail.com>
2024-03-03 22:47:04 +0530

    updated ineer text

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlbe7b0ed8": {
       "AppClientID": "string",
